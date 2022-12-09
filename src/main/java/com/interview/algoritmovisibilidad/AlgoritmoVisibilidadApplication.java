b47ec335ebe2ce03f96f60b91ac6c530092893a1
Diego Arce <diegoarcebuendia@gmail.com>
2022-12-09 20:39:14 +0100

    chore: :tada: Initial commit

---
+++ src/main/java/com/interview/algoritmovisibilidad/AlgoritmoVisibilidadApplication.java
@@ -0,0 +1,13 @@
+package com.interview.algoritmovisibilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmoVisibilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmoVisibilidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/algoritmovisibilidad/AlgoritmoVisibilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.algoritmovisibilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmoVisibilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
